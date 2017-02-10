39712685db64cd975fcadf6712557d0ddb97b70a
junhaesung <junhaesung0428@hanmail.net>
2017-02-10 04:45:16 +0000

    update mysql.js

--- server/db/mysql.js
+++ server/db/mysql.js
@@ -1,12 +1,13 @@
+var config = require('./../config/config.json');
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
 	connectionLimit : 10, 
-	host : 'localhost', 
-	port : '3306', 
-	user : 'root', 
-	password : 'Gotjd428!@', 
-	database : 'testdb'
+	host : config.db_host, 
+	port : config.db_port, 
+	user : config.db_user, 
+	password : config.db_password, 
+	database : config.db_name
 });
 
 module.exports = pool;
