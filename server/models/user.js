0bdc84994484dfb4b3b9c605b6e6b84b18f09006
junhaesung <seong0428@gmail.com>
2017-02-15 15:08:58 +0900

    fix models to run program

--- server/models/user.js
+++ server/models/user.js
@@ -1,39 +1,36 @@
 var crypto = require('crypto');
 
-//Member 모델 정의
+/**
+ *  Define a model of user table. 
+ */
 module.exports = function(sequelize, DataTypes) {
-	  var user = sequelize.define("User", {
-	    user_id : {type : DataTypes.INTEGER(15), primaryKey : true, allowNull : false},
-	    user_email : {type : DataTypes.STRING(64), allowNull : false},
-	    user_tokenid : {type : DataTypes.STRING(30), allowNull : false},
-        user_name : {type : DataTypes.STRING(30), allowNull : false},
-        user_nickname : {type : DataTypes.STRING(30), allowNull : false},
-        
-        user_gender : {type : DataTypes.INTEGER(30), allowNull : false},
-        
-        user_age : {type : DataTypes.INTEGER(30), allowNull : false},
-        
-        user_address : {type : DataTypes.STRING(30)},
-        
-        user_rank : {type : DataTypes.INTEGER(30)},
-	  }, {
-		    classMethods: {
-		    	//salt 생성
-		    	createSalt : function(){
-		    		return crypto.randomBytes(32).toString('hex');
-		    	},
-		    	//비밀번호 해시 생성
-		    	createHashPwd : function(pwd,salt){
-		    		var key = crypto.pbkdf2Sync(pwd, salt, 100000, 32, 'sha256');
+  console.log('models/user.js')
+	return sequelize.define("User", {
+	  user_id       : { type : DataTypes.INTEGER(15), primaryKey : true, allowNull : false },
+	  user_email    : { type : DataTypes.STRING(64), allowNull : false },
+	  user_tokenid  : { type : DataTypes.STRING(30), allowNull : false },
+    user_name     : { type : DataTypes.STRING(30), allowNull : false },
+    user_nickname : { type : DataTypes.STRING(30), allowNull : false },
+    user_gender   : { type : DataTypes.INTEGER(30), allowNull : false },
+    user_age      : { type : DataTypes.INTEGER(30), allowNull : false },
+    user_address  : { type : DataTypes.STRING(30) },
+    user_rank     : { type : DataTypes.INTEGER(30) }
+	}, {
+		classMethods: {
+		  //salt 생성
+		  createSalt : function() {
+		    return crypto.randomBytes(32).toString('hex');
+		  },
+		  //비밀번호 해시 생성
+		  createHashPwd : function(pwd,salt) {
+		    var key = crypto.pbkdf2Sync(pwd, salt, 100000, 32, 'sha256');
 		    		return key.toString('hex');
-		    	}
-		    },
-		    tableName: 'user',
-		    freezeTableName: true,
-		    underscored: true,
-		    timestamps: false
-		  });
-	  return user;
-	};
-
+		  }
+		},
+		tableName: 'user',
+		freezeTableName: true,
+		underscored: true,
+		timestamps: false
+  });
+};
 
