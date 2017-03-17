8e214dbfdee374c70e02fedf528699ba782ca71d
Jjin <wowlsyou@naver.com>
2017-03-18 01:36:10 +0900

    일단 올리고 한번 더 테스트 해볼게요

--- server/routes/board.js
+++ server/routes/board.js
@@ -4,62 +4,239 @@ var multer     = require('multer');
 var upload     = multer();
 var router     = express.Router();
 
-//var pool = require('./../db/mysql');
 var models     = require('../models');
 
+/*
+ // 전체 방 조회
+ router.get('/boards',entireBoard);
+ */
+
+// 전체 방 조회
+router.get('/boards', function(req, res) {
+    var result = {};
+    result["boards"] = [];
+
+    models.Board.findAll()
+        .then(function(boards) {
+            for(var i=0; i<boards.length; i++) {
+                result["boards"][i] = boards[i].dataValues;
+            }
+            res.status(200);
+            res.json(result);
+        })
+        .catch(function(err) {
+            res.status(500);
+            res.send('Something is broken!');
+        });
+});
+
+// 특정 방 조회
+router.get('/boards/:idx',certainBoard);
+
+// 방 팔로우
+router.put('/boards/:idx',followBoard);
+
+// 방 팔로우 취소
+router.put('/boardsCancel/:idx', unfollowBoard)
+
+// 인기 방 조회
+router.get('/boardsPopular',popularBoard);
+
+// 팔로우 된 방 조회
+router.get('/boardsFollowed/:idx',specialBoard);
+
 router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now())
-  next()
+    console.log('Time: ', Date.now())
+    next()
 });
 
-/*	GET board listing. 
-		GET /board        		
-*/
-router.get('/', function(req, res) {
-  // check if token is valid
+/*function entireBoard(req, res){
 
-  // check if parameter(req.body) is valid
-	
-  // send query to sql server
+ }*/
 
-  // send result to client
-});
+// 특정 방 조회 get, board_id params로 받음
+function certainBoard(req, res){
+    var idx = req.params.idx;
+    var result = {
+        status : null,
+        reason : null,
+        board  : null
+    };
+    models.Board.findById(idx).then(function(ret) {
+        if(ret == null){
+            res.status(400);
+            result.status = 'F';
+            result.reason = 'not find board';
+            res.json(result);
+        } else{
+            console.log(ret);
+            result.status = 'S';
+            result.board = ret;
+            res.json(result);
+        }
+    }, function(err) {
+        console.log(err);
+        res.status(400);
+        result.status = 'F';
+        result.reason = err.message;
+        res.json(result);
+    })
+}
 
-/*	POST /board
-*/
-router.post('/', upload.array(), function(req, res) {
-  // check if token is valid
-	
-  // check if parameter(req.body) is valid
-	
-  // send query to sql server
+// 방 팔로우 put, board_id params로 받음, bf_userid, bf_boardid body로 받음
+function followBoard(req, res){
+    var board_id = req.params.idx;
+    var bfinfo = req.body;
+    var result = {
+        board_id : null,
+        status   : null,
+        reason   : null
+    };
+    models.Board.sequelize.query('update board set board_popular = board_popular + 1 where board_id = ?',{replacements : [board_id]}).then(function(ret1){
+        models.Board_Follow.create(bfinfo).then(function(ret2){
+            res.status(200);
+            result.board_id = board_id;
+            result.status = 'S';
+            res.json(result);
+        }, function(err2){
+            res.status(400);
+            result.status = 'F';
+            result.reason = 'board_follow update failed';
+            res.json(result);
+        })
+    }, function(err1){
+        console.log(err1);
+        result.status = 'F';
+        result.reason = 'Follow failed';
+        res.json(result);
+    })
+}
 
-  // send result to client
-});
+// 방 팔로우 취소 put, board_id params로 받음, bf_userid, bf_boardid body로 받음
+function unfollowBoard(req, res){
+    var board_id = req.params.idx;
+    var bfinfo = req.body;
+    var result = {
+        board_id : null,
+        status   : null,
+        reason   : null
+    };
+    models.Board.sequelize.query('update board set board_popular = board_popular - 1 where board_id = ?',{replacements : [board_id]}).then(function(ret1){
+        models.Board_Follow.destroy({where : {bf_userid : bfinfo.bf_userid, bf_boardid : bfinfo.bf_boardid}}).then(function(ret2){
+            res.status(200);
+            result.board_id = board_id;
+            result.status = 'S';
+            res.json(result);
+        }, function(err2){
+            res.status(400);
+            result.status = 'F';
+            result.reason = 'board_follow update failed';
+            res.json(result);
+        })
+    }, function(err1){
+        console.log(err1);
+        result.status = 'F';
+        result.reason = 'Follow failed';
+        res.json(result);
+    })
+}
 
-/* PUT /board/{board_id}
-*/
-router.put('/', function(req, res) {
-  // check if token is valid
+// 인기 방 조회 get, 3개
+function popularBoard(req, res){
+    var result = {
+        reason : null,
+        board : null
+    };
 
-  // check if parameter(req.body) is valid
-	
-  // send query to sql server
+    console.log('check before query');
+    models.Board.sequelize.query('select * from board order by board_popular desc limit 3;').then(function(ret){
+        if(ret == null) {
+            res.status(400);
+            result.status = 'F';
+            result.reason = 'not find board';
+            res.json(result);
+        } else {
+            console.log(ret[0]);
+            result.status = 'S';
+            result.board = ret[0];
+            res.json(result);
+        }
+    }, function(err) {
+        console.log(err);
+        res.status(400);
+        result.status = 'F';
+        result.reason = err.message;
+        res.json(result);
+    })
+}
 
-  // send result to client
-	//res.send('PUT /user!!!!!!!!!');
-});
+/*
+ // 팔로우 된 방 조회 params로 userid 받음
+ function specialBoard(req, res){
+ var result = {
+ reason : null,
+ board  : null
+ };
+ var user_id = req.params.idx;
+ models.Board_Follow.sequelize.query('select * from board_follow where bf_userid = ?', {replacements : [user_id]}).then(function(ret1){
 
-/* DELETE /board/:{board_id}
-*/
-router.delete('/', function(req, res) {
-  // check if token is valid
+ for(var i=0; i<ret1.length; i++){
+ ret1[i].bf_boardid
+ }
+ var board_id = bf.bf_boardid;
+ console.log(board_id);
+ models.Board.findById(board_id).then(function(ret2){
+ if(ret2 == null){
+ res.status(200);
+ result.status = 'F';
+ result.reason = 'not find board';
+ res.json(result);
+ }
+ else{
+ result.status = 'S';
+ result.board = ret2;
+ res.json(result);
+ }
+ }, function(err1) {
+ res.status(400);
+ result.status = 'F';
+ result.reason = 'Add follow failed'
+ })
+ })
+ }
+ */
 
-  // check if parameter(req.body) is valid
-	
-  // send query to sql server
-
-  // send result to client
-});
+// 팔로우 된 방 조회 params로 userid 받음
+function specialBoard(req, res){
+    var result = {
+        reason : null,
+        board  : null
+    };
+    var user_id = req.params.idx;
+    models.Board_Follow.findAll({where : {bf_userid : user_id}}).then(function(ret1){
+        for(var i=0; i<ret1.length; i++) {
+            models.Board.findById(ret1[i].bf_boardid).then(function(ret2){
+                result["board"][i] = ret2[i].dataValues;
+            })
+        }
+        if(ret == null){
+            res.status(400);
+            result.status = 'F';
+            result.reason = 'not find review';
+            res.json(result);
+        } else{
+            console.log(ret);
+            result.status = 'S';
+            result.review = ret;
+            res.json(result);
+        }
+    }, function(err) {
+        console.log(err);
+        res.status(400);
+        result.status = 'F';
+        result.reason = err.message;
+        res.json(result);
+    })
+}
 
 module.exports = router;
--- server/routes/review.js
+++ server/routes/review.js
@@ -6,61 +6,238 @@ var router = express.Router();
 
 var models = require('../models');
 
+
+
 router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now())
-  next()
+    console.log('Time: ', Date.now())
+    next()
 });
 
-/*	GET review listing. 
-		GET /review        		
-*/
-router.get('/', function(req, res) {
-  console.log('GET /review');
-	// check if token is valid
-	
-	// check if parameter(req.body) is valid
-	
-	// send query to sql server
 
-	// send result to client
-  res.send('GET /review');
+// 방 안에서의 리뷰 조회 params로 방 번호를 받음
+router.get('/reviewinBoard/:idx', function(req, res) {
+    var result = {};
+    result["reviews"] = [];
+
+    console.log(result);
+    var idx = req.params.idx;
+
+
+    models.Review.findAll({where: {review_boardid : idx}})
+        .then(function(reviews) {
+            for(var i=0; i<reviews.length; i++) {
+                result["reviews"][i] = reviews[i].dataValues;
+            }
+            res.status(200);
+            res.json(result);
+        })
+        .catch(function(err) {
+            res.status(500);
+            res.send('Something is broken!');
+        });
 });
 
-/*	POST /user 
-*/
-router.post('/', upload.array(), function(req, res) {
-  // check if token is valid
-	
-  // check if parameter(req.body) is valid
-	
-  // send query to sql server
+// 인기 리뷰 조회 5개
+router.get('/reviews', popularReview);
 
-  // send result to client
-});
+// 리뷰 작성
+router.post('/reviews', regisReview);
 
-/* PUT /user/{user_id}
-*/
-router.put('/', function(req, res) {
-  // check if token is valid
+// 특정 리뷰 조회
+router.get('/reviews/:idx', certainReviewinfo);
 
-  // check if parameter(req.body) is valid
-	
-  // send query to sql server
+// 리뷰 삭제
+router.delete('/reviews/:idx', deleteReview);
 
-  // send result to client
-	//res.send('PUT /user!!!!!!!!!');
-});
+/*
+ // 리뷰 수정
+ router.put('/reviews/:idx', modifyReview);
+ */
 
-/* DELETE /user/:{user_id}
-*/
-router.delete('/', function(req, res) {
-	// check if token is valid
-	
-	// check if parameter(req.body) is valid
-	
-	// send query to sql server
+// 리뷰 좋아요
+router.put('/reviewsLike/:idx', likeReview);
 
-	// send result to client
-});
+// 리뷰 좋아요 취소
+router.put('/reviewsCancel/:idx', unlikeReview);
+
+// 리뷰 작성 post, body
+function regisReview(req, res){
+    var reviewinfo = req.body;
+    var result = {
+        review_id : null,
+        status : null,
+        reason : null
+    };
+    models.Review.create(reviewinfo).then(function(ret){
+        result.review_id = ret.review_id;
+        result.status = 'S';
+        res.json(result);
+    }, function(err){
+        result.status = 'F';
+        result.reason = err;
+        res.json(result);
+    })
+}
+
+// 특정 리뷰 조회 , get, params로 review_id 받음
+function certainReviewinfo(req, res){
+    var idx = req.params.idx;
+    var result = {
+        status : null,
+        reason : null,
+        review : null
+    };
+    models.Review.findById(idx).then(function(ret) {
+        if(ret == null){
+            res.status(400);
+            result.status = 'F';
+            result.reason = 'not find review';
+            res.json(result);
+        } else{
+            console.log(ret);
+            result.status = 'S';
+            result.review = ret;
+            res.json(result);
+        }
+    }, function(err) {
+        console.log(err);
+        res.status(400);
+        result.status = 'F';
+        result.reason = err.message;
+        res.json(result);
+    })
+}
+
+// 리뷰 삭제 delete, params로 user_id
+function deleteReview(req, res) {
+    var idx = req.params.idx;
+    var result = {
+        status : null,
+        reason : null,
+        review : null
+    };
+
+    models.Review.destroy({where: {review_id : idx}}).then(function(ret) {
+        if(ret == 1) {
+            console.log(ret);
+            result.status = 'S';
+            result.review = ret;         res.json(result);
+        }
+        else {
+            res.status(400);
+            result.status = 'F';         result.reason = 'No review to delete';
+            res.json(result);
+        }
+    }, function(err) {
+        console.log(err);
+        result.status = 'F';
+        result.reason = err.message;
+        res.json(result);
+    })
+}
+
+/*
+ // 리뷰 수정 putm params로 review_id 받음, body로 수정 내용 받음
+ function modifyReview(req, res){
+ var review_id = req.params.idx;
+ var reviewinfo = req.body;
+ var result = {
+ review_id : null,
+ status    : null,
+ reason    : null
+ };
+
+ models.Review.
+
+
+ }
+ */
+
+// 리뷰 좋아요 put, review_like++, rvr_userid, rvr_reviewid body로 받음, review_id body로 받음
+function likeReview(req, res){
+    var review_id = req.params.idx;
+    var rvrinfo = req.body;
+    var result = {
+        review_id : null,
+        status : null,
+        reason : null
+    };
+    models.Review.sequelize.query('update review set review_like = review_like + 1 where review_id = ?',
+        {replacements : [review_id]}).then(function(ret1) {    models.Review_Response.create(rvrinfo).then(function(ret2){
+            res.status(200);
+            result.review_id = review_id;
+            result.status = 'S';
+            res.json(result);
+        }, function(err2){
+            res.status(400);
+            result.status = 'F';
+            result.reason = err2;
+            res.json(result);
+        })
+        }, function(err1){
+            console.log(err1);
+            result.status = 'F';
+            result.reason = 'Add like failed';
+            res.json(result);
+        }
+    )}
+
+// 리뷰 좋아요 취소 put, review_like --, rvr_userid, rvr_reviewid body로 받음,
+function unlikeReview(req, res){
+    var review_id = req.params.idx;
+    var rvrinfo = req.body;
+    var result = {
+        review_id : null,
+        status : null,
+        reason : null
+    };
+    models.Review.sequelize.query('update review set review_like = review_like - 1 where review_id = ?',
+        {replacements : [review_id]}).then(function(ret1) {    models.Review_Response.destroy({where : {rvr_userid : rvrinfo.rvr_userid, rvr_reviewid : rvrinfo.rvr_reviewid}}).then(function(ret2){
+            res.status(200);
+            result.review_id = review_id;
+            result.status = 'S';
+            res.json(result);
+        }, function(err2){
+            res.status(400);
+            result.status = 'F';
+            result.reason = err2;
+            res.json(result);
+        })
+        }, function(err1){
+            console.log(err1);
+            result.status = 'F';
+            result.reason = 'Add like failed';
+            res.json(result);
+        }
+    )
+}
+
+//인기 리뷰 조회 get, 5개,
+function popularReview(req, res){
+    var result = {
+        reason : null,
+        review : null
+    };
+
+    models.Review.sequelize.query('select * from review order by review_like desc limit 5;').then(function(ret){
+        if(ret == null) {
+            res.status(400);
+            result.status = 'F';
+            result.reason = 'not find toilet';
+            res.json(result);
+        } else {
+            console.log(ret[0]);
+            result.status = 'S';
+            result.review = ret[0];
+            res.json(result);
+        }
+    }, function(err) {
+        console.log(err);
+        res.status(400);
+        result.status = 'F';
+        result.reason = err.message;
+        res.json(result);
+    })
+}
 
 module.exports = router;
