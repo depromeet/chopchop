3c08245b6cb6cade7704c1ab2446b19c4679ec87
Jjin <wowlsyou@naver.com>
2017-06-14 16:57:36 +0900

    리뷰 리스폰스 한번만 하도록

--- server/routes/review_response.js
+++ server/routes/review_response.js
@@ -31,28 +31,38 @@ function responseReview(req, res){
         reason    : null
     };
 
-    //body로 받은 rvr_userid, rvr_reviewid 가 review_response 테이블에 있을때 막음
-
     if      (rvrinfo.rvr_like == 1)     {rvrinfo.rvr_bad = 0; rvrinfo.rvr_report = 0;}
     else if (rvrinfo.rvr_bad == 1)      {rvrinfo.rvr_like = 0; rvrinfo.rvr_report = 0;}
     else if (rvrinfo.rvr_report == 1)   {rvrinfo.rvr_like = 0; rvrinfo.rvr_bad = 0;}
 
     models.Review_Response.create(rvrinfo).then(function(){
-            models.Review.findById(review_id).then(function(review) {
-                if      (rvrinfo.rvr_like == 1)     {return review.increment('review_like', {by: 1});}
-                else if (rvrinfo.rvr_bad == 1)      {return review.increment('review_bad', {by: 1});}
-                else if (rvrinfo.rvr_report == 1)   {return review.increment('review_report', {by: 1});}
-            }).then(function(){
-                res.status(200);
-                result.review_id = review_id;
-                result.status = 'S';
-                res.json(result);
-            }, function(err){
-                res.status(400);
-                result.status = 'F';
-                result.reason = 'response failed';
-                res.json(result);
-            })
+        models.Review_Response.findAll({where : {rvr_userid: rvrinfo.rvr_userid,
+            rvr_reviewid: rvrinfo.rvr_reviewid}}).then(function(response){
+                if(response.length > 1){
+                    models.Review_Response.destroy({
+                    where: {
+                        rvr_userid: rvrinfo.rvr_userid,
+                        rvr_reviewid: rvrinfo.rvr_reviewid,
+                        rvr_id: response[1].dataValues.rvr_id
+                    }
+                })
+            }
+        })
+
+        models.Review.findById(review_id).then(function(review) {
+            if      (rvrinfo.rvr_like == 1)     {return review.increment('review_like', {by: 1});}
+            else if (rvrinfo.rvr_bad == 1)      {return review.increment('review_bad', {by: 1});}
+            else if (rvrinfo.rvr_report == 1)   {return review.increment('review_report', {by: 1});}
+        }).then(function(){
+            res.status(200);
+            result.review_id = review_id;
+            result.status = 'S';
+            res.json(result);
+        }, function(err){
+            result.status = 'F';
+            result.reason = 'response failed';
+            res.json(result);
+        })
         }, function(err){
             result.status = 'F';
             result.reason = 'Create review_response failed';
