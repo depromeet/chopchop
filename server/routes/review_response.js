f89f95025c35f93f7a0a24259746bbc944896508
Jjin <wowlsyou@naver.com>
2017-06-16 15:19:40 +0900

    리뷰 리스폰스 중복 안되게(리얼루다가)

--- server/routes/review_response.js
+++ server/routes/review_response.js
@@ -14,15 +14,13 @@ router.use(function timeLog (req, res, next) {
     next()
 });
 
-// 리뷰 좋아요
+// 리뷰 리스폰스
 router.put('/reviewsLike',   responseReview);
 
-// 리뷰 좋아요 취소
+// 리뷰 리스폰스 취소
 router.put('/reviewsUnlike', deresponseReview);
 
-
 //리뷰 리스폰스 put, review_like++, rvr_userid, rvr_reviewid body로 받음
-
 function responseReview(req, res) {
     var rvrinfo = req.body,
 
@@ -75,41 +73,7 @@ function responseReview(req, res) {
     })
 }
 
-
-// // 리뷰 좋아요 취소 put, review_like --, rvr_userid, rvr_reviewid body로 받음,
-// function deresponseReview(req, res){
-//     var rvrinfo = req.body;
-//     var review_id = rvrinfo.rvr_reviewid;
-//     var result = {
-//         review_id : null,
-//         status : null,
-//         reason : null
-//     };
-//     models.Review_Response.destroy({where : {rvr_userid : rvrinfo.rvr_userid, rvr_reviewid : rvrinfo.rvr_reviewid}}).then(function(){
-//             models.Review.findById(review_id).then(function(review) {
-//                 if (rvrinfo.rvr_like == 1){return review.decrement('review_like', {by: 1});}
-//                 else if (rvrinfo.rvr_bad == 1){return review.decrement('review_bad', {by: 1});}
-//                 else if (rvrinfo.rvr_report == 1){return review.decrement('review_report', {by: 1});}
-//             }).then(function(){
-//                 res.status(200);
-//                 result.review_id = review_id;
-//                 result.status = 'S';
-//                 res.json(result);
-//             }, function(err){
-//                 res.status(400);
-//                 result.status = 'F';
-//                 result.reason = 'response failed';
-//                 res.json(result);
-//             })
-//         }, function(err){
-//             result.status = 'F';
-//             result.reason = 'delete review_response failed';
-//             res.json(result);
-//         }
-//     )
-// }
-
-// 리뷰 좋아요 취소 put, review_like --, rvr_userid, rvr_reviewid body로 받음,
+// 리뷰 리스폰스 취소 put, review_like --, rvr_userid, rvr_reviewid body로 받음,
 function deresponseReview(req, res) {
     var rvrinfo = req.body,
 
