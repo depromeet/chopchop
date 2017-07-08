11f2534456bb34dacbec9abbdb84b239a74b40e7
Jeemyeong Lee <jeemyeong@gmail.com>
2017-07-08 15:31:18 +0900

    Remove Container from semantic in App.js

--- web/src/App.js
+++ web/src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import RootRoute from './containers';
 
-import { Container } from 'semantic-ui-react'
-
 const App = () => {
   return (
     <Router>
-      <Container text>
-        <RootRoute/>
-      </Container>
+      <RootRoute/>
     </Router>
   );
 };
