3d49921b9b80ee843dda9786a8a28f27951a174e
Jeemyeong Lee <jeemyeong@gmail.com>
2017-07-08 21:32:58 +0900

    Remove Unnessary Lifecycle in RootRoute

--- web/src/containers/index.js
+++ web/src/containers/index.js
@@ -49,28 +49,6 @@ class RootRoute extends Component {
         }
         console.log("CONSTRUCTOR");
     }
-    componentWillMount(){
-        console.log("componentWillMount");
-    }
-    componentDidMount(){
-        console.log("componentDidMount");
-    }    
-    componentWillReceiveProps(nextProps){
-        console.log("componentWillReceiveProps: " + JSON.stringify(nextProps));
-    }
-    shouldComponentUpdate(nextProps, nextState){
-        console.log("shouldComponentUpdate: " + JSON.stringify(nextProps) + " " + JSON.stringify(nextState));
-        return true;
-    }
-    componentWillUpdate(nextProps, nextState){
-        console.log("componentWillUpdate: " + JSON.stringify(nextProps) + " " + JSON.stringify(nextState));
-    }
-    componentDidUpdate(prevProps, prevState){
-        console.log("componentDidUpdate: " + JSON.stringify(prevProps) + " " + JSON.stringify(prevState));
-    }
-    componentWillUnmount(){
-        console.log("componentWillUnmount");
-    }    
     render() {
         const authed = this.props.authReducer.authed;
         const authedLoading = this.props.authReducer.authedLoading;
